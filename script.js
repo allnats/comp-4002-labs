da641a24a6c685fdbd52a94e046a466ea018a831
Allendale Nato <anato2@academic.rrc.ca>
2026-09-07 14:47:29 -0500

    feat: replace employee table with dynamic department cards

--- script.js
+++ script.js
@@ -93,174 +93,8 @@ const employeeDeptData = [
   },
 ];
 
-const tableData = [
-  {
-    name: "Zoë Robins",
-    department: "Administration",
-  },
-  {
-    name: "Madeleine Madden",
-    department: "Administration",
-  },
-  {
-    name: "Josha Sadowski",
-    department: "Audit",
-  },
-  {
-    name: "Kate Fleetwood",
-    department: "Audit",
-  },
-  {
-    name: "Priyanka Bose",
-    department: "Banking Operations",
-  },
-  {
-    name: "Hammed Animashaun",
-    department: "Banking Operations",
-  },
-  {
-    name: "Álvaro Morte",
-    department: "Banking Operations",
-  },
-  {
-    name: "Taylor Napier",
-    department: "Banking Operations",
-  },
-  {
-    name: "Alan Simmonds",
-    department: "Banking Operations",
-  },
-  {
-    name: "Gil Cardinal",
-    department: "Communications",
-  },
-  {
-    name: "Richard J. Lewis",
-    department: "Communications",
-  },
-  {
-    name: "Randy Bradshaw",
-    department: "Corporate Services",
-  },
-  {
-    name: "Tracey Cook",
-    department: "Corporate Services",
-  },
-  {
-    name: "Lubomir Mykytiuk",
-    department: "Corporate Services",
-  },
-  {
-    name: "Dakota House",
-    department: "Facilities",
-  },
-  {
-    name: "Lori Lea Okemah",
-    department: "Facilities",
-  },
-  {
-    name: "Renae Morrisseau",
-    department: "Facilities",
-  },
-  {
-    name: "Rick Belcourt",
-    department: "Facilities",
-  },
-  {
-    name: "Selina Hanusa",
-    department: "Financial Services",
-  },
-  {
-    name: "Buffy Gaudry",
-    department: "Financial Services",
-  },
-  {
-    name: "Shaneen Ann Fox",
-    department: "Financial Services",
-  },
-  {
-    name: "Allan Little",
-    department: "Financial Services",
-  },
-  {
-    name: "Danny Rabbit",
-    department: "Financial Services",
-  },
-  {
-    name: "Jesse Ed Azure",
-    department: "Human Resources",
-  },
-  {
-    name: "Stacy Da Silva",
-    department: "Human Resources",
-  },
-  {
-    name: "Vladimír Valenta",
-    department: "Human Resources",
-  },
-  {
-    name: "Samone Sayeses-Whitney",
-    department: "Human Resources",
-  },
-  {
-    name: "Paul Coeur",
-    department: "Human Resources",
-  },
-  {
-    name: "Graham Greene",
-    department: "Information Technology",
-  },
-  {
-    name: "Sandika Evergreen",
-    department: "Information Technology",
-  },
-  {
-    name: "Jennifer Rodriguez",
-    department: "Information Technology",
-  },
-  {
-    name: "Aiyana Littlebear",
-    department: "IT Technician",
-  },
-  {
-    name: "Inara Thunderbird",
-    department: "IT Technician",
-  },
-  {
-    name: "Kaya Runningbrook",
-    department: "IT Technician",
-  },
-  {
-    name: "Elara Firehawk",
-    department: "IT Technician",
-  },
-  {
-    name: "Siona Moonflower",
-    department: "IT Technician",
-  },
-  {
-    name: "Kaiyu Greywolf",
-    department: "IT Technician",
-  },
-  {
-    name: "Ayawamat Nightwind",
-    department: "IT Technician",
-  },
-  {
-    name: "Tala Braveheart",
-    department: "IT Technician",
-  },
-  {
-    name: "Iniko Stonebear",
-    department: "IT Technician",
-  },
-  {
-    name: "Onatah Redhawk",
-    department: "IT Technician",
-  },
-];
-
 /**
+ * DEPRACATED: replaced with department cards.
  * Dynamically adds data to the table-employee-dept table.
  * @param {*} tableData
  */
@@ -281,6 +115,27 @@ const populateTable = (tableData) => {
   });
 };
 
+const populateDepartmentCards = () => {
+  const container = document.querySelector(".container-employee-dept");
+
+  employeeDeptData.forEach((data) => {
+    const departmentSection = document.createElement("article");
+    const departmentName = document.createElement("h2");
+    departmentName.innerHTML = data.department;
+    const employeeList = document.createElement("ul");
+
+    for (const employee of data.employees) {
+      const employeeName = document.createElement("li");
+      employeeName.innerHTML = employee;
+      employeeList.appendChild(employeeName);
+    }
+
+    departmentSection.appendChild(departmentName);
+    departmentSection.appendChild(employeeList);
+    container.appendChild(departmentSection);
+  });
+};
+
 /**
  * Dynamically update the copyright year.
  */
@@ -290,5 +145,6 @@ const updateCopyrightYear = () => {
 };
 
 // Add data
-populateTable(tableData);
+// populateTable(tableData);
+populateDepartmentCards();
 updateCopyrightYear();
