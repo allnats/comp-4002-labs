d72510e688a840d6f37e2db1d7301a85d6b49e96
Allendale Nato <anato2@academic.rrc.ca>
2026-09-07 16:43:16 -0500

    feat: add EmployeeDirectory component with styling and data integration

--- src/components/landing/EmployeeDirectory.tsx
+++ src/components/landing/EmployeeDirectory.tsx
@@ -1,13 +1,46 @@
 import Header from "../common/header/Header";
 import Footer from "../common/footer/Footer";
+import data from "../../data/employee-dept.json";
+import "./EmployeeDirectory.css";
 
-function EmployeeDirectory() {
+interface Department {
+  department: string;
+  employees: string[];
+}
+
+const employeeDeptData: Department[] = data as Department[];
+
+export default function EmployeeDirectory() {
   return (
     <>
       <Header />
+      <ListDepartments data={employeeDeptData} />
       <Footer />
     </>
   );
 }
 
-export default EmployeeDirectory;
+function ListDepartments({ data }: { data: Department[] }) {
+  return (
+    <div className="container-employee-dept">
+      {data.map((department) => (
+        <article key={department.department}>
+          <h2>{department.department}</h2>
+          <ListEmployees names={department.employees} />
+        </article>
+      ))}
+    </div>
+  );
+}
+
+function ListEmployees({ names }: { names: string[] }) {
+  return (
+    <ul>
+      {names.map((name, idx) => (
+        <li key={`${name}${idx}`}>{name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export { EmployeeDirectory };
