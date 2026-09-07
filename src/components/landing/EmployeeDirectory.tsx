ba79b9263039248f71d94258fedf9d015f745c55
Allendale Nato <anato2@academic.rrc.ca>
2026-09-07 13:29:12 -0500

    feat: add EmployeeDirectory component

---
+++ src/components/landing/EmployeeDirectory.tsx
@@ -0,0 +1,13 @@
+import Header from "../common/header/Header";
+import Footer from "../common/footer/Footer";
+
+function EmployeeDirectory() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default EmployeeDirectory;
