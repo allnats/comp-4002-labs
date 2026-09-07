127e42e6baa7634467f87ccb22bde3133fe09a47
Allendale Nato <anato2@academic.rrc.ca>
2026-09-07 15:47:15 -0500

    feat: implement Header and Footer components with styling

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,5 +1,11 @@
+import "./Footer.css";
+
 function Footer() {
-  return <div></div>;
+  return (
+    <footer>
+      <p>&copy; PiXELL River Financial {new Date().getFullYear()}</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,5 +1,18 @@
+import "./Header.css";
+import pixellRiverLogo from "../../../assets/pixell-river-logo.svg";
+
 function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <img src={pixellRiverLogo} alt="PiXELL River Logo" />
+      <div className="title">
+        <h1 className="header-title">PiXELL River Employee Directory</h1>
+        <p className="greeting">
+          List of all employees by their department sections.
+        </p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
