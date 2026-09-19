d0ad2343576a91f854f6ecdd403d9064d8ef40a9
Allendale Nato <anato2@academic.rrc.ca>
2026-09-19 12:28:18 -0500

    refactor: update employee and department data structure in JSON and components

--- src/components/landing/EmployeeDirectory.tsx
+++ src/components/landing/EmployeeDirectory.tsx
@@ -1,14 +1,11 @@
 import Header from "../common/header/Header";
 import Footer from "../common/footer/Footer";
 import data from "../../data/employee-dept.json";
+import type Department from "../../models/department";
+import type Employee from "../../models/employee";
 import "./EmployeeDirectory.css";
 
-interface Department {
-  department: string;
-  employees: string[];
-}
-
-const employeeDeptData: Department[] = data as Department[];
+const employeeDeptData: Department[] = data;
 
 export default function EmployeeDirectory() {
   return (
@@ -24,8 +21,8 @@ function ListDepartments({ data }: { data: Department[] }) {
   return (
     <div className="container-employee-dept">
       {data.map((department) => (
-        <article key={department.department}>
-          <h2>{department.department}</h2>
+        <article key={department.name}>
+          <h2>{department.name}</h2>
           <ListEmployees names={department.employees} />
         </article>
       ))}
@@ -33,11 +30,13 @@ function ListDepartments({ data }: { data: Department[] }) {
   );
 }
 
-function ListEmployees({ names }: { names: string[] }) {
+function ListEmployees({ names }: { names: Employee[] }) {
   return (
     <ul>
-      {names.map((name, idx) => (
-        <li key={`${name}${idx}`}>{name}</li>
+      {names.map((employee, idx) => (
+        <li key={`${employee.firstName}${employee.lastName ?? ""}${idx}`}>
+          {employee.firstName} {employee.lastName}
+        </li>
       ))}
     </ul>
   );
