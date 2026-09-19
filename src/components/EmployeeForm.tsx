51f7cd5c0864327c314edcceb336fc09dd257b9f
Allendale Nato <anato2@academic.rrc.ca>
2026-09-19 13:00:11 -0500

    feat: add input fields in Employee form

--- src/components/EmployeeForm.tsx
+++ src/components/EmployeeForm.tsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import type { SubmitEvent } from "react";
 
 function EmployeeForm() {
-  return <form onSubmit={handleFormSubmit}></form>;
-}
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
-function handleFormSubmit(e: SubmitEvent<HTMLFormElement>) {
-  e.preventDefault();
+  function handleFormSubmit(e: SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(`${firstName} ${lastName}`);
+  }
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <fieldset>
+        <legend>Add a new Employee</legend>
+
+        {/* First name Input field */}
+        <label htmlFor="fname">First Name</label>
+        <input
+          type="text"
+          value={firstName}
+          name="fname"
+          onChange={(e) => setFirstName(() => e.target.value)}
+        />
+
+        {/* Last name input field. */}
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(() => e.target.value)}
+        />
+      </fieldset>
+    </form>
+  );
 }
 
 export default EmployeeForm;
