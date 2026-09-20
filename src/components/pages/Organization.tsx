69c1f1d931e53c47e814c34a969d63745bf2a09e
Allendale Nato <anato2@academic.rrc.ca>
2026-09-19 20:21:39 -0500

    feat: update Header component to accept dynamic title and greeting

--- src/components/pages/Organization.tsx
+++ src/components/pages/Organization.tsx
@@ -1,28 +1,33 @@
 import type Role from "../../models/role";
 import data from "./../../data/org-roles.json";
 
+import "./Organization.css";
+import Header from "../common/header/Header";
 const organizationData: Role[] = data;
 
 function Organization() {
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>Role</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {organizationData.map((data) => (
-            <tr key={`${data.firstName}${data.lastName}`}>
-              <td>{data.role}</td>
-              <td>{`${data.firstName} ${data.lastName}`}</td>
+      <Header title="Organization" greeting="Leadership and Management" />
+      <div className="org-table-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Role</th>
+              <th>Name</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {organizationData.map((data) => (
+              <tr key={`${data.firstName}${data.lastName}`}>
+                <td>{data.role}</td>
+                <td>{`${data.firstName} ${data.lastName}`}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
