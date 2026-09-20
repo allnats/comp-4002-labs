e4db0e1ebf34759df2d59d2cd21bbacdefbc3a4d
Allendale Nato <anato2@academic.rrc.ca>
2026-09-19 20:21:46 -0500

    feat: replace Header with Navbar in Layout component and add Organization CSS styles

--- src/components/common/layout/Layout.tsx
+++ src/components/common/layout/Layout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
 
-import Header from "../header/Header";
+import Navbar from "./Navbar";
 import Footer from "../footer/Footer";
 
 function Layout() {
   return (
     <>
-      <Header />
+      <Navbar />
       <Outlet />
       <Footer />
     </>
